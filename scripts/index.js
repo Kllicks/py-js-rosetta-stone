36069f6fb28b9be4a34ede05d4f8772dc658ccba
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-04 15:23:12 -0400

    adds another file to redo more python projects in js

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,20 @@
-// Yep. It's JavaScript
+//hard-coding instead of pompting
+// because in real life, when you want
+//user input, you'll create an html form
 
-// hello.py
-//console.log("hello javascript what is up")
+// using backticks for hipster strings.
+const name = `kyle`;
+const subject = `computer science`;
 
-//its lame.
-
-// //using var -> variable must be declared
-// //and camelCase not snake-case
-// var promptText = 'What is your name';
-// var userName = prompt(promptText);
-
-// //uses string concatenation
-// var greeting = 'Hello, ' + userName + '!';
-// console.log(greeting);
+// const finalText = `${name}'s favorite subject in school is ${subject}
 
 
-// ======== now, using SEXY syntax ===========
-// // using let and const
-// // Rule of thumb: use const until you get an error then use let
-// const promptText = 'What is your name?';
-// const userName = prompt(promptText);
-// // const makes variables that can only be defined once
-// // userName = userName + 'is a bozo';
-// const greeting = 'Hello, ' + userName + '!';
-// console.log(greeting);
 
-//keyword let allows you to redefine
-const promptText = 'What is your name?';
-let userName = prompt(promptText);
-userName = userName + ' is a bozo';
-//string interpolation
-// 1. replace with backticks
-// 2. remove the + signs
-// 3. wrap your variables in ${}
-// you can use backticks instead of '' quotes if you want
-const greeting = `Hello, ${userName}!`; //backticks and ${cashcurly}
-console.log(greeting);
+// how lame.
+// `;
 
+//or
+
+const finalText = `${name}'s favorite subject in school is ${subject} \n\n\n\nhow lame.`;
+
+console.log(finalText);
