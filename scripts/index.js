7adeeba55c8c46cc6a2d3db5f174324041f82984
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-05 11:13:08 -0400

    adds tip amount exercise and turns it into js

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,32 @@
-// start with a hard-coded value until you know your program works
-// then switch to prompting the user
+// Configuration ===================================
+const LEVEL_GOOD = `good`;
+const LEVEL_FAIR = `fair`;
+const LEVEL_BAD = `bad`;
 
-let theDay = prompt("Day (0-6)?");
+const TIP_GOOD = 0.2;
+const TIP_FAIR = 0.15;
+const TIP_BAD = 0.1;
 
-// to convert from a string to a number,
-// use parseInt and provide base 10 as an argument
-theDay = parseInt(theDay, 10);
+// Processing ======================================
 
-//const theDay = prompt("Day (0-6)?");
-// const theDay = 3;
-let dayName = `invalid choice`;
+// start with const until something breaks
+const totalBill = 100;
+const levelOfService = `good`;
 
-// must you parentheses for your condition even if its one
-// loose equality is ==
-// strict equality is ===
-// by default, use strict equality ===
-// "threequals"
-if (theDay === 0) { // and curly braces!
-    //you can leave off the curly braces 
-    //but your body can only have one statement
-    //any statement after the first will get run
-    //regardless of wheter the condition is true or not
-    dayName = `Sunday`;
-} else if (theDay === 1) {
-    dayName = `Monday`;
-} else if (theDay === 2) {
-    dayName = `Tuesday`;
-} else if (theDay === 3) {
-    dayName = `Wednesday`;
-} else if (theDay === 4) {
-    dayName = `Thursday`;
-} else if (theDay === 5) {
-    dayName = `Friday`;
-} else if (theDay === 6) {
-    dayName = `Saturday`;
+// If you leave off const or let or var
+// you end up with a var
+let tip = 0.0;
+
+if (levelOfService === LEVEL_GOOD) {
+    tip = totalBill * TIP_GOOD;
+} else if (levelOfService === LEVEL_FAIR) {
+    tip = totalBill * TIP_FAIR;
+} else if (levelOfService === LEVEL_BAD) {
+    tip = totalBill * TIP_BAD;
+} else {
+    console.log(`Invalid Input. `);
 }
-// the body of an if statement is surrounded by curly braces. 
-// and does not require a semi-colon after the curly braces
 
-console.log(dayName);
+// Result===========================================
+console.log(`Tip amount: ${tip}`);
+console.log(`Total amount: ${totalBill + tip}`);
