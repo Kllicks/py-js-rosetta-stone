cb58744fac60a693917ff4948c95a671f6041181
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-08 14:24:22 -0400

    adds leetspeak in js

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,34 @@
-// Arrays aka python lists
-// objects aka python dictionaries
-// _custom_ functions
-// classes optional
+// leetspeak
 
-// for (let i = 0; i < 5; i++){
-//     console.log(`woooooooooooof`);
-// }
+const text = `you wish you were batman`;
 
-const whoami = `i'm batman`;
-// in python, this would be: whoami[0:1]
-const first = whoami.slice(0, 1).toUpperCase();
-// in python, this would be: whoami[1:]
-const rest = whoami.slice(1);
+const lettersToConvert = ["A", "E","G","I","O","S","T"];
+const numbers = [4,3,6,1,0,5,7];
 
-console.log(first + rest);
+const textToTranslate = text.toUpperCase();
+let translation = ``;
 
-console.log(`The length of the string is ${whoami.length}`);
+// ES6 style array iteration
+// Basically the equivalent of pythong's for loop
+// for (let letter of text.toUpperCase()) {
+
+// "block scoping"
+for (let letter of textToTranslate) {
+    // console.log(letter);
+    // Check to see if the current letter is in `lettersToConvert`
+    
+    let index = lettersToConvert.indexOf(letter);
+    // console.log(lettersToConvert.indexOf(letter))
+    if (index !== -1) {
+        // if so, grab the index
+
+        // use the index to grab the corresponding item from `numbers`
+        translation = translation + numbers[index];
+    } else {
+        translation = translation + letter;
+    }
+
+    // else, just use the original letter
+}
+
+console.log(translation);
